9408add359567255fa0adaed347ea11c9b2d7083
Takashi Iwai <tiwai@suse.de>
2015-02-02 15:44:54 +0100

    misc: Add attribute groups
    Add groups field to struct miscdevice for passing the attribute groups
    at device creation.  In this way, the driver can avoid the manual call
    of device_create_file() after the device registration, which is
    basically a racy operation, in addition to the reduction of manual
    device_remove_file() calls.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/miscdevice.h
+++ include/linux/miscdevice.h
@@ -53,6 +53,7 @@
 #define MISC_DYNAMIC_MINOR	255
 
 struct device;
+struct attribute_group;
 
 struct miscdevice  {
 	int minor;
@@ -61,6 +62,7 @@ struct miscdevice  {
 	struct list_head list;
 	struct device *parent;
 	struct device *this_device;
+	const struct attribute_group **groups;
 	const char *nodename;
 	umode_t mode;
 };
