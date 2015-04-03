a28cc5c77f058fa54bef66d865dc5413059ad9cb
Ian Morris <ipm@chirality.org.uk>
2015-04-03 09:17:27 +0100

    ipv4: coding style: comparison for inequality with NULL
    The ipv4 code uses a mixture of coding styles. In some instances check
    for non-NULL pointer is done as x != NULL and sometimes as x. x is
    preferred according to checkpatch and this patch makes the code
    consistent by adopting the latter form.
    
    No changes detected by objdiff.
    
    Signed-off-by: Ian Morris <ipm@chirality.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: I4d846b7461b52ac8def9ea0ea1b5e299f7ddde25

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -1520,7 +1520,7 @@ int udp_queue_rcv_skb(struct sock *sk, struct sk_buff *skb)
 
 		/* if we're overly short, let UDP handle it */
 		encap_rcv = ACCESS_ONCE(up->encap_rcv);
-		if (skb->len > sizeof(struct udphdr) && encap_rcv != NULL) {
+		if (skb->len > sizeof(struct udphdr) && encap_rcv) {
 			int ret;
 
 			/* Verify checksum before giving to encap */
@@ -1864,7 +1864,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 		sk = __udp4_lib_lookup_skb(skb, uh->source, uh->dest, udptable);
 	}
 
-	if (sk != NULL) {
+	if (sk) {
 		int ret;
 		#if defined(CONFIG_KNOX_NCM)
 /* START_OF_KNOX_NPA */
