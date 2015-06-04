06efea20b17b306ac57455fcf637413e3a81b7f1
Eric W. Biederman <ebiederm@xmission.com>
2015-06-04 09:43:11 -0500

    mnt: fs_fully_visible enforce noexec and nosuid if !SB_I_NOEXEC
    The filesystems proc and sysfs do not have executable files do not
    have exectuable files today and portions of userspace break if we do
    enforce nosuid and noexec consistency of nosuid and noexec flags
    between previous mounts and new mounts of proc and sysfs.
    
    Add the code to enforce consistency of the nosuid and noexec flags,
    and use the presence of SB_I_NOEXEC to signal that there is no need to
    bother.
    
    This results in a completely userspace invisible change that makes it
    clear fs_fully_visible can only skip the enforcement of noexec and
    nosuid because it is known the filesystems in question do not support
    executables.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -3742,6 +3742,8 @@ static bool fs_fully_visible(struct file_system_type *type, int *new_mnt_flags)
 		    ((mnt->mnt->mnt_flags & MNT_ATIME_MASK) != (new_flags & MNT_ATIME_MASK)))
 			continue;
 #else
+		int mnt_flags;
+
 		if (mnt->mnt.mnt_sb->s_type != type)
 			continue;
 
@@ -3751,17 +3753,30 @@ static bool fs_fully_visible(struct file_system_type *type, int *new_mnt_flags)
 		if (mnt->mnt.mnt_root != mnt->mnt.mnt_sb->s_root)
 			continue;
 
+		/* Read the mount flags and filter out flags that
+		 * may safely be ignored.
+		 */
+		mnt_flags = mnt->mnt.mnt_flags;
+		if (mnt->mnt.mnt_sb->s_iflags & SB_I_NOEXEC)
+			mnt_flags &= ~(MNT_LOCK_NOSUID | MNT_LOCK_NOEXEC);
+
 		/* Verify the mount flags are equal to or more permissive
 		 * than the proposed new mount.
 		 */
-		if ((mnt->mnt.mnt_flags & MNT_LOCK_READONLY) &&
+		if ((mnt_flags & MNT_LOCK_READONLY) &&
 		    !(new_flags & MNT_READONLY))
 			continue;
-		if ((mnt->mnt.mnt_flags & MNT_LOCK_NODEV) &&
+		if ((mnt_flags & MNT_LOCK_NODEV) &&
 		    !(new_flags & MNT_NODEV))
 			continue;
-		if ((mnt->mnt.mnt_flags & MNT_LOCK_ATIME) &&
-		    ((mnt->mnt.mnt_flags & MNT_ATIME_MASK) != (new_flags & MNT_ATIME_MASK)))
+		if ((mnt_flags & MNT_LOCK_NOSUID) &&
+		    !(new_flags & MNT_NOSUID))
+			continue;
+		if ((mnt_flags & MNT_LOCK_NOEXEC) &&
+		    !(new_flags & MNT_NOEXEC))
+			continue;
+		if ((mnt_flags & MNT_LOCK_ATIME) &&
+		    ((mnt_flags & MNT_ATIME_MASK) != (new_flags & MNT_ATIME_MASK)))
 			continue;
 #endif
 		/* This mount is not fully visible if there are any
@@ -3783,16 +3798,11 @@ static bool fs_fully_visible(struct file_system_type *type, int *new_mnt_flags)
 				goto next;
 		}
 		/* Preserve the locked attributes */
-
-#ifdef CONFIG_RKP_NS_PROT
-		*new_mnt_flags |= mnt->mnt->mnt_flags & (MNT_LOCK_READONLY | \
-							MNT_LOCK_NODEV    | \
-							MNT_LOCK_ATIME);
-#else
-		*new_mnt_flags |= mnt->mnt.mnt_flags & (MNT_LOCK_READONLY | \
-							MNT_LOCK_NODEV    | \
-							MNT_LOCK_ATIME);
-#endif
+		*new_mnt_flags |= mnt_flags & (MNT_LOCK_READONLY | \
+					       MNT_LOCK_NODEV    | \
+					       MNT_LOCK_NOSUID   | \
+					       MNT_LOCK_NOEXEC   | \
+					       MNT_LOCK_ATIME);
 		visible = true;
 		goto found;
 	next:	;
