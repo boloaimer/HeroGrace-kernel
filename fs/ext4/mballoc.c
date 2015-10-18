f9994a86a978943a4b522f64e23563514ccff75c
Daeho Jeong <daeho.jeong@samsung.com>
2015-10-17 22:28:21 -0400

    ext4: fix xfstest generic/269 double revoked buffer bug with bigalloc
    When you repeatly execute xfstest generic/269 with bigalloc_1k option
    enabled using the below command:
    
    "./kvm-xfstests -c bigalloc_1k -m nodelalloc -C 1000 generic/269"
    
    you can easily see the below bug message.
    
    "JBD2 unexpected failure: jbd2_journal_revoke: !buffer_revoked(bh);"
    
    This means that an already revoked buffer is erroneously revoked again
    and it is caused by doing revoke for the buffer at the wrong position
    in ext4_free_blocks(). We need to re-position the buffer revoke
    procedure for an unspecified buffer after checking the cluster boundary
    for bigalloc option. If not, some part of the cluster can be doubly
    revoked.
    
    Signed-off-by: Daeho Jeong <daeho.jeong@samsung.com>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -4873,6 +4873,7 @@ void ext4_free_blocks(handle_t *handle, struct inode *inode,
 		int is_metadata = flags & EXT4_FREE_BLOCKS_METADATA;
 
 		for (i = 0; i < count; i++) {
+			cond_resched();
 			if (is_metadata)
 				bh = sb_find_get_block(inode->i_sb, block + i);
 			ext4_forget(handle, is_metadata, inode, bh, block + i);
