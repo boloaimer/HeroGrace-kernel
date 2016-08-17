6639f15bcabdc65b52d6d340ba4326c9b4bc04c8
Kees Cook <keescook@chromium.org>
2016-08-17 14:42:08 -0700

    BACKPORT: list: Split list_add() debug checking into separate function
    (cherry-picked from d7c816733d501b59dbdc2483f2cc8e4431fd9160)
    
    Right now, __list_add() code is repeated either in list.h or in
    list_debug.c, but the only differences between the two versions
    are the debug checks. This commit therefore extracts these debug
    checks into a separate __list_add_valid() function and consolidates
    __list_add(). Additionally this new __list_add_valid() function will stop
    list manipulations if a corruption is detected, instead of allowing for
    further corruption that may lead to even worse conditions.
    
    This is slight refactoring of the same hardening done in PaX and Grsecurity.
    
    Change-Id: Ib2cdcf961fed22766ff4a6d2872634fc805fd633
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Satya Tangirala <satyat@google.com>

--- lib/list_debug.c
+++ lib/list_debug.c
@@ -20,23 +20,22 @@
 bool __list_add_valid(struct list_head *new, struct list_head *prev,
 		      struct list_head *next)
 {
-	if (WARN(next->prev != prev,
-		"list_add corruption. next->prev should be "
-		"prev (%p), but was %p. (next=%p).\n",
-		prev, next->prev, next) ||
-	    WARN(prev->next != next,
-		"list_add corruption. prev->next should be "
-		"next (%p), but was %p. (prev=%p).\n",
-		next, prev->next, prev) ||
-	    WARN(new == prev || new == next,
-	         "list_add double add: new=%p, prev=%p, next=%p.\n",
-	         new, prev, next))
-		BUG_ON(1);
-
-	next->prev = new;
-	new->next = next;
-	new->prev = prev;
-	prev->next = new;
+	if (unlikely(next->prev != prev)) {
+		WARN(1, "list_add corruption. next->prev should be prev (%p), but was %p. (next=%p).\n",
+			prev, next->prev, next);
+		return false;
+	}
+	if (unlikely(prev->next != next)) {
+		WARN(1, "list_add corruption. prev->next should be next (%p), but was %p. (prev=%p).\n",
+			next, prev->next, prev);
+		return false;
+	}
+	if (unlikely(new == prev || new == next)) {
+		WARN(1, "list_add double add: new=%p, prev=%p, next=%p.\n",
+			new, prev, next);
+		return false;
+	}
+	return true;
 }
 EXPORT_SYMBOL(__list_add_valid);
 
