f958d68b6a6e36190522a7d722a4eff9deb3c287
Yuchung Cheng <ycheng@google.com>
2016-09-21 16:16:15 -0700

    tcp: properly account Fast Open SYN-ACK retrans
    Since the TFO socket is accepted right off SYN-data, the socket
    owner can call getsockopt(TCP_INFO) to collect ongoing SYN-ACK
    retransmission or timeout stats (i.e., tcpi_total_retrans,
    tcpi_retransmits). Currently those stats are only updated
    upon handshake completes. This patch fixes it.
    
    Signed-off-by: Yuchung Cheng <ycheng@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: Soheil Hassas Yeganeh <soheil@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: TALU <taluemail272@gmail.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -3704,6 +3704,8 @@ int tcp_rtx_synack(struct sock *sk, struct request_sock *req)
 	if (!res) {
 		TCP_INC_STATS_BH(sock_net(sk), TCP_MIB_RETRANSSEGS);
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_TCPSYNRETRANS);
+		if (unlikely(tcp_passive_fastopen(sk)))
+			tcp_sk(sk)->total_retrans++;
 	}
 	return res;
 }
