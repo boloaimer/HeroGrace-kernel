c9327c0378cc0b8b2f1d8b81253f9c5d980c6afd
TALU <taluemail272@gmail.com>
2020-07-17 23:46:26 +0100

    usb: gadget: function: import most changes from A600FNXXU5CTD1.
    Merge most usb gadget changes from 3.18-Q

--- drivers/usb/gadget/function/u_ether.c
+++ drivers/usb/gadget/function/u_ether.c
@@ -783,11 +783,11 @@ static int alloc_tx_buffer(struct eth_dev *dev)
 #endif
 	list_for_each(act, &dev->tx_reqs) {
 		req = container_of(act, struct usb_request, list);
-		if (!req->buf)
+		if (!req->buf) {
 			req->buf = kmalloc(dev->tx_req_bufsize,
 						GFP_ATOMIC);
-			if (!req->buf)
 				goto free_buf;
+		}
 #ifdef CONFIG_USB_NCM_ACCUMULATE_MULTPKT
 			if(dev->port_usb->is_fixed) {
 				memcpy(req->buf,dev->port_usb->header,dev->port_usb->header_len);
@@ -889,8 +889,13 @@ static netdev_tx_t eth_start_xmit(struct sk_buff *skb,
 	list_del(&req->list);
 
 	/* temporarily stop TX queue when the freelist empties */
+#ifdef CONFIG_USB_RNDIS_MULTIPACKET
+	if (list_empty(&dev->tx_reqs) && (dev->tx_skb_hold_count >= (dev->dl_max_pkts_per_xfer -1)))
+		netif_stop_queue(net);
+#else
 	if (list_empty(&dev->tx_reqs))
 		netif_stop_queue(net);
+#endif
 	spin_unlock_irqrestore(&dev->req_lock, flags);
 
 	/* no buffer copies needed, unless the network stack did it
