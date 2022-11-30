8c3ba6e2868c2ac3260816db988f18af68c499d7
Derek <nielson925@gmail.com>
2022-11-29 18:32:03 -0700

    net dst.c: Fix some warnings
    Change-Id: I81e0bc756840c533d543c974f33017edbf1a8693

--- net/core/dst.c
+++ net/core/dst.c
@@ -347,7 +347,7 @@ static struct dst_ops md_dst_ops = {
 	.family =		AF_UNSPEC,
 };
 
-static int dst_md_discard_sk(struct sock *sk, struct sk_buff *skb)
+static int dst_md_discard_sk(struct sk_buff *skb)
 {
 	WARN_ONCE(1, "Attempting to call output on metadata dst\n");
 	kfree_skb(skb);
