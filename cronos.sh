27a193eae5796b6141f9cb8163dd405dcc738bb8
boloaimer <q2591187844@gmail.com>
2025-11-16 19:30:56 +0800

    cronos.sh: Change the compiler paths
    Add the correct paths for GCC 11 and GCC 12 compilers.

--- cronos.sh
+++ cronos.sh
@@ -82,8 +82,8 @@ CR_DTB_MOUNT=$CR_DTS_ONEUI
 CR_RAMDISK=$CR_RAMDISK_Q
 CR_HALLIC="1"
 # Compiler Paths
-CR_GCC12=~/Android/Toolchains/aarch64-linux-gnu-12.x/bin/aarch64-linux-gnu-
-CR_GCC11=~/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+CR_GCC12=~/Exynos8890-source-tools/compiler/gcc/gcc-arm-12.3-arm64/bin/aarch64-none-linux-gnu-
+CR_GCC11=~/Exynos8890-source-tools/compiler/gcc/gcc-arm-11.3-arm64/bin/aarch64-none-linux-gnu-
 CR_GCC9=~/Exynos8890-source-tools/compiler/gcc/gcc-arm-9.2-arm64/bin/aarch64-none-linux-gnu-
 CR_CLANG=~/Exynos8890-source-tools/compiler/clang/proton-clang13-llvm-20210522/bin
 CR_GCC4=~/Exynos8890-source-tools/compiler/gcc/google-gcc-4.9/bin/aarch64-linux-android-
