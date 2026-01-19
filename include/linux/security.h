56ebe24dc78e222ded47ee2209b5bc34e0ce27b3
boloaimer <q2591187844@gmail.com>
2026-01-20 00:22:57 +0800

    Reapply "selinux: add a skb_owned_by() hook"
    This reverts commit 95e679a7159d5dde5ec7741b7ebf6f7abf4ef374.

--- include/linux/security.h
+++ include/linux/security.h
@@ -1466,6 +1466,7 @@ static inline void security_free_mnt_opts(struct security_mnt_opts *opts)
  *	@ctxlen points to the place to put the length of @ctx.
  * This is the main security structure.
  */
+
 /* prototypes */
 extern int security_init(void);
 
@@ -2431,6 +2432,8 @@ int security_tun_dev_attach_queue(void *security);
 int security_tun_dev_attach(struct sock *sk, void *security);
 int security_tun_dev_open(void *security);
 
+void security_skb_owned_by(struct sk_buff *skb, struct sock *sk);
+
 #else	/* CONFIG_SECURITY_NETWORK */
 static inline int security_unix_stream_connect(struct sock *sock,
 					       struct sock *other,
@@ -2622,6 +2625,11 @@ static inline int security_tun_dev_open(void *security)
 {
 	return 0;
 }
+
+static inline void security_skb_owned_by(struct sk_buff *skb, struct sock *sk)
+{
+}
+
 #endif	/* CONFIG_SECURITY_NETWORK */
 
 #ifdef CONFIG_SECURITY_NETWORK_XFRM
